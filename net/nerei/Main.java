894990ec8f80f6e06cc24cb77ab6c8f39c414c93
rl <rl@nerei.net>
2016-09-06 16:30:05 +0200

    init

---
+++ net/nerei/Main.java
@@ -0,0 +1,20 @@
+package net.nerei;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+
+
+        in.close();
+    }
+}
