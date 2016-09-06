9e7a2a2e5ba8d28039b2e47530e5e5572d11a364
rl <rl@nerei.net>
2016-09-06 17:57:13 +0200

    Circular Array - submitted

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -12,8 +12,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        int k = in.nextInt();
+        short q = in.nextShort();
 
+        int[] a = new int[n];
 
+        for (int i=0; i<n; i++)
+            a[(i + k) % n] = in.nextInt();
+
+        try {
+            for (int j = 0; j < q; j++) {
+                System.out.format("%d%n", a[in.nextInt()]);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("Index is out of bounds!");
+        }
 
         in.close();
     }
