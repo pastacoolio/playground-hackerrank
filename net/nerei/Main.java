7391ca2a70fa6ccb51eda739a826d39a91d00b40
rl <rl@nerei.net>
2016-09-06 17:18:26 +0200

    Library fine - submitted

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -8,8 +8,38 @@ import java.util.regex.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        int dr = in.nextInt();
+        int mr = in.nextInt();
+        int yr = in.nextInt();
+        int de = in.nextInt();
+        int me = in.nextInt();
+        int ye = in.nextInt();
 
-        scan.close();
+        int fine;
+
+        if (yr > ye)
+            fine = 10000;
+        else {
+            if (yr < ye) {
+                fine = 0;
+            } else { // yr == ye
+                if (mr < me) {
+                    fine = 0;
+                } else if (mr > me) {
+                    fine = (mr - me) * 500;
+                } else { // mr == me
+                    if (dr <= de) {
+                        fine =0;
+                    } else {
+                        fine = (dr - de) * 15;
+                    }
+                 }
+            }
+        }
+
+        System.out.println(fine);
+
+        in.close();
     }
 }
