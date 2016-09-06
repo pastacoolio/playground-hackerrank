e315fa079133d766f8776881ae935ec457aae129
rl <rl@nerei.net>
2016-09-06 16:45:00 +0200

    TotalCost Submitted

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -5,16 +5,23 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
 
+        // Write your calculation code here.
+        float tip = (float) (mealCost * tipPercent / 100.0f);
+        float tax = (float) (mealCost * taxPercent / 100.0f);
+        float total = (float) (mealCost + tip + tax);
+        // cast the result of the rounding operation to an int and save it as totalCost
+        int totalCost = Math.round(total);
 
-
-        in.close();
+        // Print your result
+        System.out.format("The total meal cost is %d dollars.%n", totalCost);
     }
 }
