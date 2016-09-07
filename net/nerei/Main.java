26f9290b2b529bc694aadad5d2123336911b8d42
rl <rl@nerei.net>
2016-09-07 12:00:15 +0200

    CountCamelCase - submitted

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -9,12 +9,46 @@ import java.util.regex.*;
 
 public class Main {
 
+    private static final String[] EMPTY_ARRAY = new String[0];
+
+    private static String[] splitByCharacterType(String str, boolean camelCase) {
+        if (str == null) {
+            return null;
+        }
+        if (str.length() == 0) {
+            return EMPTY_ARRAY;
+        }
+        char[] c = str.toCharArray();
+        List<String> list = new ArrayList<>();
+        int tokenStart = 0;
+        int currentType = Character.getType(c[tokenStart]);
+        for (int pos = tokenStart + 1; pos < c.length; pos++) {
+            int type = Character.getType(c[pos]);
+            if (type == currentType) {
+                continue;
+            }
+            if (camelCase && type == Character.LOWERCASE_LETTER && currentType == Character.UPPERCASE_LETTER) {
+                int newTokenStart = pos - 1;
+                if (newTokenStart != tokenStart) {
+                    list.add(new String(c, tokenStart, newTokenStart - tokenStart));
+                    tokenStart = newTokenStart;
+                }
+            } else {
+                list.add(new String(c, tokenStart, pos - tokenStart));
+                tokenStart = pos;
+            }
+            currentType = type;
+        }
+        list.add(new String(c, tokenStart, c.length - tokenStart));
+        return list.toArray(new String[list.size()]);
+    }
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        Scanner sc = new Scanner(System.in);
+        String[] elements = splitByCharacterType(sc.nextLine(), true);
 
+        System.out.println(elements.length);
 
-
-        in.close();
+        sc.close();
     }
 }
