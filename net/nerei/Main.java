7879b994f8573f8de5492a0b05f3b53e1608f68d
rl <rl@nerei.net>
2016-09-07 16:27:08 +0200

    Bigger is better

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -9,12 +9,61 @@ import java.util.regex.*;
 
 public class Main {
 
+    private static ArrayList<String> permutations(String s) {
+        if (s == null) {
+            return null;
+        }
+
+        ArrayList<String> resultList = new ArrayList<>();
+
+        if (s.length() < 2) {
+            resultList.add(s);
+
+            return resultList;
+        }
+
+        int length = s.length();
+        char currentChar;
+
+        for (int i = 0; i < length; i++) {
+            currentChar = s.charAt(i);
+
+            String subString = s.substring(0, i) + s.substring(i + 1);
+
+            ArrayList<String> subPermutations = permutations(subString);
+
+            for (String item : subPermutations) {
+                resultList.add(currentChar + item);
+            }
+        }
+
+        return resultList;
+    }
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
 
 
+        for(int i=0;i<t;i++) {
+            String s = sc.nextLine();
+            ArrayList sorted = permutations(s);
+            Collections.sort(sorted);
 
-        in.close();
+            ListIterator iter = sorted.listIterator();
+            while(iter.hasNext()){
+                if((s.compareTo(String.valueOf(iter.next()))) >= 0){
+                    iter.remove();
+                }
+            }
+
+            if (sorted.size() == 0)
+                System.out.println("no answer");
+            else
+                System.out.println(sorted.get(0));
+
+        }
+        sc.close();
     }
 }
