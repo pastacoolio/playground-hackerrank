ba6036daa855d3bf766252c0874eb806c3ea62e6
rl <rl@nerei.net>
2016-09-09 01:18:34 +0200

    init

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -9,53 +9,27 @@ import java.util.regex.*;
 
 public class Main {
 
-    private static void swap(char[] arr, int i, int j){
-        char t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        int testcases = sc.nextInt();
         sc.nextLine();
+
 
         try {
             try (PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8")) {
 
 
 
-                for(int i1=0;i1<t;i1++) {
+                for(int i1=0;i1<testcases;i1++) {
+                    int v=0;
+                    int w;
                     String line = sc.nextLine();
 
                     if (line.length() <= 100 && line.length() >= 1) {
-                        char[] ind = line.toCharArray();
+                        int t = (v | (v - 1)) + 1;
+                        w = t | ((((t & -t) / (v & -v)) >> 1) - 1);
 
-                        //ind is an array of integers
-                        for(int tail = ind.length - 1;tail > 0;tail--){
-                            if (ind[tail - 1] < ind[tail]){//still increasing
-
-                                //find last element which does not exceed ind[tail-1]
-                                int s = ind.length - 1;
-                                while(ind[tail-1] >= ind[s])
-                                    s--;
-
-                                swap(ind, tail-1, s);
-
-                                //reverse order of elements in the tail
-                                for(int i = tail, j = ind.length - 1; i < j; i++, j--){
-                                    swap(ind, i, j);
-                                }
-                                break;
-                            }
-                        }
-                        if (line.compareTo( String.valueOf(ind) ) == 0) {
-                            //System.out.println("no answer");
-                            writer.println("no answer");
-                        } else {
-                            //System.out.println(ind);
-                            writer.println(ind);
-                        }
+                        writer.println(w);
                     }
                 }
                 sc.close();
