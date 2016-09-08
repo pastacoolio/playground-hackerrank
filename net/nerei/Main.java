86bfeb986d1c60c0bd24a31b82a96c6d55f77495
rl <rl@nerei.net>
2016-09-08 02:43:11 +0200

    Bigger is better - TestCase3 Data added

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -20,38 +20,53 @@ public class Main {
         int t = sc.nextInt();
         sc.nextLine();
 
+        try {
+            try (PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8")) {
 
-        for(int i1=0;i1<t;i1++) {
-            String line = sc.nextLine();
 
-            if (line.length() <= 100 && line.length() > 1) {
-                char[] ind = line.toCharArray();
 
-                //ind is an array of integers
-                for(int tail = ind.length - 1;tail > 0;tail--){
-                    if (ind[tail - 1] < ind[tail]){//still increasing
+                for(int i1=0;i1<t;i1++) {
+                    String line = sc.nextLine();
 
-                        //find last element which does not exceed ind[tail-1]
-                        int s = ind.length - 1;
-                        while(ind[tail-1] >= ind[s])
-                            s--;
+                    if (line.length() <= 100 && line.length() > 1) {
+                        char[] ind = line.toCharArray();
 
-                        swap(ind, tail-1, s);
+                        //ind is an array of integers
+                        for(int tail = ind.length - 1;tail > 0;tail--){
+                            if (ind[tail - 1] < ind[tail]){//still increasing
 
-                        //reverse order of elements in the tail
-                        for(int i = tail, j = ind.length - 1; i < j; i++, j--){
-                            swap(ind, i, j);
+                                //find last element which does not exceed ind[tail-1]
+                                int s = ind.length - 1;
+                                while(ind[tail-1] >= ind[s])
+                                    s--;
+
+                                swap(ind, tail-1, s);
+
+                                //reverse order of elements in the tail
+                                for(int i = tail, j = ind.length - 1; i < j; i++, j--){
+                                    swap(ind, i, j);
+                                }
+                                break;
+                            }
                         }
-                        break;
+                        if (line.compareTo( String.valueOf(ind) ) == 0) {
+                            //System.out.println("no answer");
+                            writer.println("no answer");
+                        } else {
+                            //System.out.println(ind);
+                            writer.println(ind);
+                        }
                     }
                 }
-                if (line.compareTo( String.valueOf(ind) ) == 0) {
-                    System.out.println("no answer");
-                } else {
-                    System.out.println(ind);
-                }
+                sc.close();
+
+                //writer.println("The second line");
+                writer.close();
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
-        sc.close();
     }
 }
