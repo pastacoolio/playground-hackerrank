01e9488ea85bee63d46945a21c0cd45357646eeb
rl <rl@nerei.net>
2016-09-08 23:54:30 +0200

    How old are you - submitted

--- net/nerei/Person.java
+++ net/nerei/Person.java
@@ -8,17 +8,29 @@ public class Person {
 
     public Person(int initialAge) {
         // Add some more code to run some checks on initialAge
-        if (initialAge)
-            System.out.println(/*Insert correct print statement here*/);
+        if (initialAge < 0) {
+            this.age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        } else {
+            this.age = initialAge;
+        }
+
     }
 
     public void amIOld() {
-        // Write code determining if this person's age is old and print the correct statement:
-        System.out.println(/*Insert correct print statement here*/);
+        if (this.age < 13) {
+            System.out.println("You are young.");
+        }
+        else if (this.age >= 13 && this.age <18) {
+            System.out.println("You are a teenager.");
+        }
+        else {
+            System.out.println("You are old.");
+        }
     }
 
     public void yearPasses() {
-        // Increment this person's age.
+        this.age++;
     }
 
     public static void main(String[] args) {
