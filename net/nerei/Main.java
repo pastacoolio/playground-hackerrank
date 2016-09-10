d816de15d8669a37bca95c01c7b6ea67978ba289
rl <rl@nerei.net>
2016-09-10 15:37:02 +0200

    30DayOfCode - Init

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -10,11 +10,10 @@ import java.util.regex.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        try (Scanner in = new Scanner(System.in)) {
+            int n = in.nextInt();
 
-
-
-        in.close();
+            in.close();
+        }
     }
 }
