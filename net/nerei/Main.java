1bbbe55b136aaed6d168cc2bdc26cd08ee605e2d
rl <rl@nerei.net>
2016-09-11 15:40:35 +0200

    submitted OK

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -12,8 +12,15 @@ public class Main {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             int n = in.nextInt();
-
+            int[] arr = new int[n];
+            for (int i=0; i<n; i++){
+                arr[i] = in.nextInt();
+            }
             in.close();
+
+            for(int j=n-1;i>=0; i--) {
+                System.out.format("%s ", arr[j]);
+            }
         }
     }
 }
