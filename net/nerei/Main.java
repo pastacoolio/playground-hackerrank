70b5e3286fae4a3d3a22322d9f6ac9c2f78446c0
rl <rl@nerei.net>
2016-09-11 15:55:54 +0200

    submitted OK

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -10,11 +10,18 @@ import java.util.regex.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        try (Scanner in = new Scanner(System.in)) {
+            int testCases = in.nextInt();
+            for(int testCase=0;testCase<testCases;testCase++) {
+                int px=in.nextInt();
+                int py=in.nextInt();
+                int qx=in.nextInt();
+                int qy=in.nextInt();
 
+                System.out.format("%d %d%n", qx+(qx-px), qy+(qy-py));
+            }
 
-
-        in.close();
+            in.close();
+        }
     }
 }
