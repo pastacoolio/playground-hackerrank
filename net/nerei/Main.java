c1a03f3dd5dddbd7589b1461584f2b7f2207ebba
rl <rl@nerei.net>
2016-09-12 22:28:41 +0200

    submitted OK

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -9,10 +9,25 @@ import java.util.regex.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String []argh){
         try (Scanner in = new Scanner(System.in)) {
             int n = in.nextInt();
-
+            HashMap<String, Integer> phoneBook = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                String name = in.next();
+                int phone = in.nextInt();
+                // Write code here
+                phoneBook.put(name, phone);
+            }
+            while (in.hasNext()) {
+                String s = in.next();
+                // Write code here
+                if(phoneBook.containsKey(s)) {
+                    System.out.format("%s=%d%n",s,phoneBook.get(s));
+                } else {
+                    System.out.println("Not found");
+                }
+            }
             in.close();
         }
     }
