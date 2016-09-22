8ba48c730a488e5c5c1b925b51306e4ae9e6760c
pastacoolio <pasta.cool@nerei.net>
2016-09-22 08:54:16 +0200

    submitted OK

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -11,7 +11,13 @@ public class Main {
 
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
-            int n = in.nextInt();
+            String S = in.next();
+            try {
+                int num = Integer.parseInt(S);
+                System.out.print(num);
+            } catch (Exception e) {
+                System.out.print("Bad String");
+            }
 
             in.close();
         }
