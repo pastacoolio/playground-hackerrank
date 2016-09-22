27897e533490eeb961f799726c00250bbfd4c3e4
pastacoolio <pasta.cool@nerei.net>
2016-09-22 08:43:32 +0200

    submitted OK

--- net/nerei/Main.java
+++ net/nerei/Main.java
@@ -4,26 +4,47 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
-import java.util.regex.*;
-
-
-
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+    public static  Node insert(Node head,int data) {
+        //Complete this method
+
+        Node temp2 = head;
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            return newNode;
+        } else {
+            // Traverse down to end of the list.
+            while (temp2.next != null)
+                temp2 = temp2.next;
+
+            // Append at the end of the list.
+            temp2.next = newNode;
         }
+        return head;
+    }
+
+
+    public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
         sc.close();
-
-        Difference difference = new Difference(a);
-
-        difference.computeDifference();
-
-        System.out.print(difference.maximumDifference);
     }
 }
---
+++ net/nerei/Node.java
@@ -0,0 +1,15 @@
+package net.nerei;
+
+/**
+ * Created by rl on 21/09/16.
+ */
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
